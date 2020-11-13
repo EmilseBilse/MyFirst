a4dc3d37b468d263572d9e0916ca633c5fb972d6
oleeriksen <oe@easv.dk>
2020-11-14 00:00:03 +0100

    Update MainActivity.kt

--- app/src/main/java/com/easv/oe/myfirst/MainActivity.kt
+++ app/src/main/java/com/easv/oe/myfirst/MainActivity.kt
@@ -2,11 +2,10 @@ package com.easv.oe.myfirst
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
