90c738318f187d9ed2239db003698104a93d2588
oleeriksen <oe@easv.dk>
2021-01-27 23:10:48 +0100

    div

--- app/src/main/java/com/easv/oe/myfirst/Greetings.kt
+++ app/src/main/java/com/easv/oe/myfirst/Greetings.kt
@@ -8,7 +8,6 @@ class Greetings {
     val generator = Random()
     val greetingText = arrayOf("Hello", "Hey", "Dear", "Have a nice day")
 
-
     public fun get(): String {
         val idx = generator.nextInt(greetingText.size)
         return greetingText[idx];
