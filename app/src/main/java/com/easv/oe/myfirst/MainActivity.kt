114ed00ddd1d3bd9341d5b83e6626010f21a8068
oleeriksen <oe@easv.dk>
2022-01-25 22:26:28 +0100

    upgraded

--- app/src/main/java/com/easv/oe/myfirst/MainActivity.kt
+++ app/src/main/java/com/easv/oe/myfirst/MainActivity.kt
@@ -2,7 +2,9 @@ package com.easv.oe.myfirst
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 
 class MainActivity : AppCompatActivity() {
@@ -12,17 +14,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        /* The following declarations are done by the
-        import of kotlinx.android.synthetic.main.activity_main.*
 
-        val tvHeader = findViewById<TextView>(R.id.tvHeader)
+        /* The following declarations are done by the
+        import of kotlinx.android.synthetic.main.activity_main.*/
         val btnOK = findViewById<Button>(R.id.btnOK)
-        val etName = findViewById<EditText>(R.id.etName)
-        */
+
         btnOK.setOnClickListener { v -> onClickOK() }
     }
 
     fun onClickOK(){
+        val etName = findViewById<EditText>(R.id.etName)
+        val tvHeader = findViewById<TextView>(R.id.tvHeader)
         val gr = mGreetings.get()
         val name = etName.text.toString()
         tvHeader.text = "$gr $name"
